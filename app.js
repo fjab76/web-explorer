dad0f167a35633ba7db76d6fbb6eb4fec2e27668
Francisco Alvarez <fjab76@gmail.com>
2015-11-22 22:12:49 +0000

    refactoring of depth level browsing algorithm

--- app.js
+++ app.js
@@ -10,35 +10,3 @@ var urlSeed = 'http://stackoverflow.com/';
 pageReader.getAllLinks(urlSeed, 0, function(err,links){
 	console.log(links);
 });
-/*
-var protocol = url.parse(urlSeed).protocol;
-
-console.log('urlSeed:'+urlSeed);
-console.log('protocol:'+protocol);
-
-var httpObject;
-if(protocol==='http:'){
-	httpObject = http;
-}
-else if(protocol==='https:'){
-	httpObject = https;
-}
-
-httpObject.get(urlSeed,function(response){
-
-		var page = '';
-		response.on('data', function (chunk) {
-		    page += chunk;
-		  });
-
-		response.on('end', function () {
-			console.log(page);
-		    pageReader.getAllLinks(page, 0, function(links){
-		    	console.log(links);
-		    });
-		  });
-
-		
-	}).on('error', function(e) {
-		  	console.log(e.message);
-		});*/
